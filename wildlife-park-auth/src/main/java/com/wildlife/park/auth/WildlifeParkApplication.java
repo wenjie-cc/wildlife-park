8131e95840fff329dc64c42547f7065af6054ae2
wenjie <wuwenjie@kloudtron.com>
2020-02-19 14:29:53 +0800

    init project

---
+++ wildlife-park-auth/src/main/java/com/wildlife/park/auth/WildlifeParkApplication.java
@@ -0,0 +1,13 @@
+package com.wildlife.park.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildlifeParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildlifeParkApplication.class, args);
+	}
+
+}
---
+++ wildlife-park-server/src/test/java/com/wildfife/park/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildfife.park.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
