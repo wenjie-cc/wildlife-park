51082de1399e0a5f641f147c005c60f49b9a592c
yanhunter <18601786493@163.com>
2020-04-13 20:54:14 +0800

    init jpa

--- ddl/01-CREATE-USER.sql
+++ ddl/01-CREATE-USER.sql
@@ -1,2 +1,23 @@
-create user WLPARK identified by WLP041105;
+create tablespace my_data
+    datafile 'E:\APP\18601\ORADATA\ORCL\my_data.DBF'
+    size 200m
+    autoextend on
+    next 100m maxsize 2048m
+    extent management local;
+
+
+create temporary tablespace my_data_temp
+    tempfile 'E:\APP\18601\ORADATA\ORCL\my_data_temp.dbf'
+    size 200m
+    autoextend on
+    next 100m maxsize 2048m
+    extent management local;
+
+
+create user WLPARK identified by WLP041105
+    default tablespace my_data
+    temporary tablespace my_data_temp
+    quota unlimited on my_data;
+
 grant connect, resource to WLPARK;
+
