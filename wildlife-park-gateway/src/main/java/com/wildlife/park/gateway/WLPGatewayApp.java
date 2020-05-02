3cc13baa493ba7f3ba5b2a01822bee7cdf80e7fa
yanhunter <18601786493@163.com>
2020-05-02 23:15:35 +0800

    exclude datasource autoconfig in gateway

--- wildlife-park-gateway/src/main/java/com/wildlife/park/gateway/WLPGatewayApp.java
+++ wildlife-park-gateway/src/main/java/com/wildlife/park/gateway/WLPGatewayApp.java
@@ -2,9 +2,10 @@ package com.wildlife.park.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 public class WLPGatewayApp {
 
